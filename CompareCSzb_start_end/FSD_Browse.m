46aed9d1c89483160c9e3eec3bda91f35bb0e972
bhqasx <815810395@qq.com>
2017-05-08 09:20:28 +0800

    subday函数作为单独文件

--- CompareCSzb_start_end/FSD_Browse.m
+++ CompareCSzb_start_end/FSD_Browse.m
@@ -45,21 +45,4 @@ end
 end
 
 
-%----------------------------------------------------------
-function nd=subday(mm,yy)
-%计算水文年中第km个月的天数
-
-if mm==2
-    if (mod(yy,4)==0&&mod(yy,100)~=0)||(mod(yy,400)==0)
-        nd=29;
-    else
-        nd=28;
-    end
-elseif mm==4||mm==6||mm==9||mm==11
-    nd=30;
-else
-    nd=31;
-end
-
-end
     
---
+++ CompareCSzb_start_end/subday.m
@@ -0,0 +1,16 @@
+function nd=subday(mm,yy)
+%计算日历年中第mm个月的天数
+
+if mm==2
+    if (mod(yy,4)==0&&mod(yy,100)~=0)||(mod(yy,400)==0)
+        nd=29;
+    else
+        nd=28;
+    end
+elseif mm==4||mm==6||mm==9||mm==11
+    nd=30;
+else
+    nd=31;
+end
+
+end
