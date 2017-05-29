ea8e0e5edd3f9f49de6af4578c8e5f366a8a3b7d
bhqasx <815810395@qq.com>
2017-05-30 00:02:35 +0800

    为了生成日级配所需的函数

--- FormatReadConvert/PrintHydroCalendar.m
+++ FormatReadConvert/PrintHydroCalendar.m
@@ -3,5 +3,22 @@ function date=PrintHydroCalendar(ystart,yend)
 
 date=[];
 for ky=ystart:1:yend
-    
+    for km=1:1:12
+        if km<=6
+            mm=km+6;
+            nd=subday(mm,ky);
+        else
+            mm=km-6;
+            nd=subday(mm,ky+1);
+        end
+        
+        date_1m=zeros(nd,3);
+        date_1m(:,1)=ky;
+        date_1m(:,2)=mm;
+        date_1m(:,3)=[1:nd].';
+        
+        date=[date;date_1m];
+    end
 end
+
+
