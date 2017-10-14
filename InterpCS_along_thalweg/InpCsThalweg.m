3ca9bf6bee6b4c1618ee5b42697ca5e329164eec
bhqasx <815810395@qq.com>
2017-10-14 18:54:55 +0800

    改为在制定沿程距离插值

--- InterpCS_along_thalweg/InpCsThalweg.m
+++ InterpCS_along_thalweg/InpCsThalweg.m
@@ -1,9 +1,10 @@
+function CSnew=InpCsThalweg(dist_inp)
 %把各断面起点距0点全部对齐在深泓线上后对断面插值
 %地形文件格式应与一维Fortran程序中要求的相同
-clear all;
-global CSold CSnew;
+%dist_inp: 列向量，插入断面的沿程距离，单位km
 
 total_nodes=0;
+dist_inp=dist_inp*1000;        %转换为m
 %--------------------------------输入旧地形资料----------------------------
 button=questdlg('请打开旧的地形文件','Guide','Yes');
 if ~strcmp(button,'Yes')
@@ -66,37 +67,44 @@ for ii=1:1:ncs
 end
 
 %----------------------------------------------------------------------------------
-%每次在相邻两个断面间插值处一个新断面
+%每个新断面用距离其最近的相邻两个已知断面插值
 %-----------------------------------------------------------------------------------
-a=cell(1,2*ncs-1);
+ncs_inp=size(dist_inp,1);         %插入断面个数
+a=cell(1,ncs+ncs_inp);
 CSnew=struct('nodes',a,'x',a,'zb',a,'min_x',a,'min_z',a,'dist',a);
 total_nodes=0;
-for ii=1:1:ncs-1
-    CSnew(2*ii-1)=CSold(ii);
-    total_nodes=total_nodes+CSold(ii).nodes;
+[dist_new,idx_cs]=sort([cs_dist; dist_inp]);       %重新排列沿程距离
+for ii=1:1:ncs+ncs_inp
+    if idx_cs(ii)<=ncs
+        CSnew(ii)=CSold(idx_cs(ii));
+    else
+        for jj=1:1:ncs
+            if (cs_dist(jj)<dist_new(ii))&&(cs_dist(jj+1)>dist_new(ii))
+                np_inp=round((CSold(jj).nodes+CSold(jj+1).nodes)/2);            %插值断面的节点数
+                Xp=zeros(np_inp,2);
+                Xp(:,1)=dist_new(ii);
+                y1=interp1(cs_dist(jj:jj+1),[CSold(jj).x(1);CSold(jj+1).x(1)],dist_new(ii));
+                y2=interp1(cs_dist(jj:jj+1),[CSold(jj).x(end);CSold(jj+1).x(end)],dist_new(ii));
+                Xp(:,2)=linspace(y1, y2,np_inp);    
+                
+                Zp=griddatan(data_xy,data_val,Xp);             %插值点上的高程   
+                %插值结果输入到新地形集合中
+                CSnew(ii).nodes=np_inp;
+                CSnew(ii).dist=dist_new(ii)/1000;
+                CSnew(ii).x=Xp(:,2);
+                CSnew(ii).zb=Zp;
+            end
+        end
+    end
     
-    dist_inp=(cs_dist(ii)+cs_dist(ii+1))/2;         %插值处的距离
-    np_inp=round((CSold(ii).nodes+CSold(ii+1).nodes)/2);            %插值断面的节点数
-    Xp=zeros(np_inp,2);
-    Xp(:,1)=dist_inp;
-    Xp(:,2)=linspace((CSold(ii).x(1)+CSold(ii+1).x(1))/2, (CSold(ii).x(end)+CSold(ii+1).x(end))/2,np_inp);
-    
-    Zp=griddatan(data_xy,data_val,Xp);             %插值点上的高程
-    %插值结果输入到新地形集合中
-    CSnew(2*ii).nodes=np_inp;
-    CSnew(2*ii).dist=dist_inp;
-    CSnew(2*ii).x=Xp(:,2);
-    CSnew(2*ii).zb=Zp;
-    total_nodes=total_nodes+np_inp;
+    total_nodes=total_nodes+CSnew(ii).nodes;   
 end
-CSnew(2*ncs-1)=CSold(ncs);
-total_nodes=total_nodes+CSold(ncs).nodes;
 
 %--------------------------------绘制插值结果-------------------------------------
 data_xy=zeros(total_nodes,2);
 data_val=zeros(total_nodes,1);
 count=0;
-for ii=1:1:2*ncs-1
+for ii=1:1:ncs+ncs_inp
     data_xy(count+1:count+CSnew(ii).nodes,1)=CSnew(ii).dist;
     data_xy(count+1:count+CSnew(ii).nodes,2)=CSnew(ii).x;
     data_val(count+1:count+CSnew(ii).nodes)=CSnew(ii).zb;
@@ -111,7 +119,7 @@ fclose(file_id);
 file_id=fopen('CS_interplated.dat', 'w');
 fprintf(file_id,'%s\n','断面地形输入');
 fprintf(file_id,'%d\n',2*ncs-1);
-for ii=1:1:2*ncs-1
+for ii=1:1:ncs+ncs_inp
     fprintf(file_id,'%s\n',['CS',num2str(ii)]);
     fprintf(file_id,'%s\t%s\n', '100', '100');
     fprintf(file_id,'%d\t%d\n', CSnew(ii).nodes, 100);
@@ -131,8 +139,8 @@ end
 
 fprintf(file_id, '%s\n', '各断面沿程距离');
 fprintf(file_id, '%s\n', 'i      DistLg(i)');
-for ii=1:1:2*ncs-1
-    fprintf(file_id, '%3d\t%6.3f\n', ii, CSnew(ii).dist/1000);                         %沿程距离
+for ii=1:1:ncs+ncs_inp
+    fprintf(file_id, '%3d\t%6.3f\n', ii, CSnew(ii).dist);                         %沿程距离
 end
 
 fclose(file_id);
